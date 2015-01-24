d2f0c42c1e48f13ab93042a011d8bd41c56c6678
Kishan Reddy <kishanreddy.kethu@gmail.com>
2015-01-25 04:40:18 +0530

    Add uread support

---
+++ uread.py
@@ -0,0 +1,25 @@
+import urllib2
+from bs4 import BeautifulSoup
+import pyisbn
+import re
+import time
+import mechanize
+def uread(isbn):
+	if len(isbn) == 10:
+		u_link = "http://www.uread.com/search-books/"+pyisbn.Isbn10(isbn).convert(code='978')
+	else:
+		u_link = "http://www.uread.com/search-books/"+isbn
+	br = mechanize.Browser()
+	#br.set_all_readonly(False)    # allow everything to be written to
+	br.set_handle_robots(False)   # ignore robots
+	br.set_handle_refresh(False)  # can sometimes hang without this
+	br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]           # [('User-agent', 'Firefox')]
+	response = br.open(u_link)
+	u_soup = BeautifulSoup(response.read())
+	u_tag = u_soup.select('#ctl00_phBody_ProductDetail_lblourPrice')
+	if not u_tag:
+		return 'NA'
+	else:
+		u_tag = u_tag[0]
+	u_price = re.findall(r'</span>(.*?)</label>',str(u_tag))[0]
+	return u_price
