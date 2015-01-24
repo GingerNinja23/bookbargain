2d268d1205f6b1e0385dcc4de9d0707894173e0b
Kishan Reddy <kishanreddy.kethu@gmail.com>
2015-01-25 03:33:53 +0530

    Add Amazon and Infibeam

---
+++ amazon.py
@@ -0,0 +1,17 @@
+import urllib2
+from bs4 import BeautifulSoup
+import pyisbn
+import re
+import time
+import mechanize
+isbn = '9789380501932'
+a_link = "http://www.amazon.in/dp/"+pyisbn.Isbn(isbn).convert(code='978')
+br = mechanize.Browser()
+#br.set_all_readonly(False)    # allow everything to be written to
+br.set_handle_robots(False)   # ignore robots
+br.set_handle_refresh(False)  # can sometimes hang without this
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]           # [('User-agent', 'Firefox')]
+response = br.open(a_link)
+a_soup = BeautifulSoup(response.read())
+a_class = a_soup.findAll('span',class_="price3P")[0]
+a_price = re.findall(r'</span> (.*?)</span>',str(a_class))[0]
