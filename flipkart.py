909e6516580ccacc0be9d7c8be50cb6495871e8b
Teja Theegala <sriteja23@gmail.com>
2015-01-25 03:53:21 +0530

    Flipkart now returns dict

--- flipkart.py
+++ flipkart.py
@@ -5,10 +5,8 @@ import mechanize
 import re
 
 
-
-
-#Getting the Source
 def flipkart(isbn):
+	#Getting the Source
 	f_link = "http://www.flipkart.com/search?q="+isbn
 	br = mechanize.Browser()
 	br.set_handle_robots(False)   # ignore robots
@@ -16,26 +14,39 @@ def flipkart(isbn):
 	br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]# [('User-agent', 'Firefox')]
 	response = br.open(f_link)
 
-	#Getting the soup
+	#Creating the soup
 
 	f_soup = BeautifulSoup(response.read())
 
 	#Getting the data
-	f_name_temp =(f_soup.find(itemprop="name"))
+	f_title_temp =(f_soup.find(itemprop="name"))
 	if(f_name_temp):
-		f_name = f_name_temp.string
+		f_title = f_title_temp.string
+
 		f_price_temp = f_soup.find_all("span",class_="selling-price omniture-field")[0]
 		if(f_price_temp):
-			f_price=f_price_temp.string
+			f_price=(f_price_temp.string).replace('Rs. ','')
 		else:
 			f_price="Not Available"
+
 		f_author_temp = (f_soup.find_all(href=re.compile("/author/[\w\s,.$><?@#$%^&*()_:-]+"))[0])
 		if(f_author_temp):
 			f_author = f_author_temp.string
 		else:
 			f_author="Unknown"
+
 		f_img_temp = (f_soup.find_all("img",class_="productImage  current")[0])['data-src']
 		if(f_img_temp):
 			f_img=f_img_temp
+		else:
+			f_img = "Not Available"
 
-	return f_price
+	else:
+		f_title = :"Not Available"
+		f_price="Not Available"
+		f_author="Unknown"
+		f_img = "Not Available"
+
+	flip_dict = {'name':f_title,'price':'f_price','author':f_author,'image_url':f_img}
+
+	return flip_dict
