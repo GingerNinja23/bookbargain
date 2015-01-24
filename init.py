bf56560a6fd396d33a331252287b6288b9570981
Teja Theegala <sriteja23@gmail.com>
2015-01-25 05:07:22 +0530

    Converd ISBN 10 to 13 for Flipkart.py

--- init.py
+++ init.py
@@ -2,7 +2,7 @@ from flipkart import flipkart
 from amazon import amazon
 from infibeam import infibeam
 from uread import uread
-isbn = '9789380501932'
+isbn = '9381384688'
 print 'Flipkart',flipkart(isbn)
 print 'Amazon',amazon(isbn)
 print 'Infibeam',infibeam(isbn)
