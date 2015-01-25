71fbd82e5e792b9b7c6c3315c068a9028dd7427a
Kishan Reddy <kishanreddy.kethu@gmail.com>
2015-01-25 06:29:17 +0530

    Add crossword

--- init.py
+++ init.py
@@ -3,9 +3,11 @@ from amazon import amazon
 from infibeam import infibeam
 from uread import uread
 from landmark import landmark
+from crossword import crossword
 isbn = '9789380501932'
 print 'Flipkart',flipkart(isbn)
 print 'Amazon',amazon(isbn)
 print 'Infibeam',infibeam(isbn)
 print 'uread',uread(isbn)
 print 'landmark',landmark(isbn)
+print 'crossword',crossword(isbn)
