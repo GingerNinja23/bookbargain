a8475f1f4dd9eabf3089e7ac6a6161f950eeb359
Kishan Reddy <kishanreddy.kethu@gmail.com>
2015-01-25 06:03:55 +0530

    Check landmark

--- init.py
+++ init.py
@@ -2,8 +2,10 @@ from flipkart import flipkart
 from amazon import amazon
 from infibeam import infibeam
 from uread import uread
+from landmark import landmark
 isbn = '9789380501932'
 print 'Flipkart',flipkart(isbn)
 print 'Amazon',amazon(isbn)
 print 'Infibeam',infibeam(isbn)
 print 'uread',uread(isbn)
+print 'landmark',landmark(isbn)
