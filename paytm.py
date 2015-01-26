69b5efb144841b85839df97ae0e640f503c492d7
Kishan Reddy <kishanreddy.kethu@gmail.com>
2015-01-27 02:53:03 +0530

    support phantomjs fot paytm

--- paytm.py
+++ paytm.py
@@ -1,4 +1,3 @@
-import contextlib
 import selenium.webdriver as webdriver
 import bs4 as bs
 import pyisbn
@@ -9,22 +8,23 @@ def paytm(isbn):
 		p_link = 'https://paytm.com/shop/search/?q='+pyisbn.Isbn10(isbn).convert(code='978')+'&sort_price=1'
 	else:		
 		p_link = 'https://paytm.com/shop/search/?q='+isbn+'&sort_price=1'
-	with contextlib.closing(webdriver.Firefox()) as driver:
-	    try:
-	    	driver.get(p_link)
-	    	content = driver.page_source
-	    	# print content
-	    except Exception, e:
-	    	print 'error'
-	    	return 'NA'
-	    	driver.quit()
-	    	content = ''
-	    soup = bs.BeautifulSoup(content)
-	    p_class = soup.findAll('span',class_="border-radius ng-binding")
-	    # print p_class
-	    if not p_class:
-	    	return 'NA'
-	    else:
-	    	p_class = p_class[0]
-	    	p_price = re.findall(r'>Rs (.*?)</span>',str(p_class))[0]
-	    	return p_price
+	# print p_link
+	driver = webdriver.PhantomJS('phantomjs',service_args=['--ssl-protocol=any'])
+	# driver = webdriver.Firefox()
+	driver.get(p_link)
+	content = driver.page_source
+	# print content	
+	driver.quit()
+	soup = bs.BeautifulSoup(content)
+	p_class = soup.findAll('span',class_="border-radius ng-binding")
+	# print p_class
+	# print p_class
+	if not p_class:
+		return 'NA'	
+	else:
+		p_class = p_class[0]
+	try:
+		p_price = re.findall(r'>Rs (.*?)</span>',str(p_class))[0]
+		return {'price':p_price,'url':p_link}
+	except Exception, e:
+		return {'price':'NA','url':p_link}
