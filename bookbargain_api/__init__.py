5fe4bf4f1c8357abbf8c520d41574a4fcf051172
Teja Theegala <sriteja23@gmail.com>
2015-04-02 13:45:41 -0700

    Fixed Import problem

--- bookbargain_api/__init__.py
+++ bookbargain_api/__init__.py
@@ -1,16 +1,22 @@
 from flask import Flask, request
 import multiprocessing.pool as mpool
 
-from .modules import flipkart,amazon,uread,crossword,landmark,infibeam
+from .modules.flipkart import flipkart
+from .modules.amazon import amazon
+from .modules.uread import uread
+from .modules.crossword import crossword
+from .modules.landmark import landmark
+from .modules.infibeam import infibeam
+
 app = Flask(__name__)
 
-def worker(target, isbn):
+def worker((target, isbn)):
     return target(isbn)
 
 @app.route("/api")
 def api():
 	pool = mpool.ThreadPool()
-	isbn = int(request.args.get('isbn'))
+	isbn = request.args.get('isbn')
 	args = [(target, isbn) for target in (flipkart,amazon,uread,crossword,landmark,infibeam)]
 	result = pool.map(worker, args)
 	flip = result[0]
