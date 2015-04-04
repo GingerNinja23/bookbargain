85394b078286ab83472329443f8340b094853dc8
Teja Theegala <sriteja23@gmail.com>
2015-04-04 19:40:28 +0530

    Update __init__.py

--- bookbargain_api/__init__.py
+++ bookbargain_api/__init__.py
@@ -26,21 +26,24 @@ def api():
 
 
 	data = OrderedDict()
-	data["Book Name"]=result[0]['name']
-	data["Author"]=result[0]['author']
-	data["Description"]=result[0]['desc']
-	data["Image"]=result[0]['image_url']
-	data["Flipkart Price"]=result[0]['price']
-	data["Flipkart URL"]=result[0]['url']
-	data["Amazon Price"]=result[1]['price']
-	data["Amazon URL"]=result[1]['url']
-	data["URead Price"]=result[2]['price']
-	data["URead URL"]=result[2]['url']
-	data["Crossword Price"]=result[3]['price']
-	data["Crossword URL"]=result[3]['url']
-	data["Landmark on the net Price"]=result[4]['price']
-	data["Landmark on the net URL"]=result[4]['url']
-	data["Infibeam Price"]=result[5]['price']
-	data["Infibeam URL"]=result[5]['url']
-
+	if(result[0]['name]!="NA"):
+		data["Status"]="Success"
+		data["Book Name"]=result[0]['name']
+		data["Author"]=result[0]['author']
+		data["Description"]=result[0]['desc']
+		data["Image"]=result[0]['image_url']
+		data["Flipkart Price"]=result[0]['price']
+		data["Flipkart URL"]=result[0]['url']
+		data["Amazon Price"]=result[1]['price']
+		data["Amazon URL"]=result[1]['url']
+		data["URead Price"]=result[2]['price']
+		data["URead URL"]=result[2]['url']
+		data["Crossword Price"]=result[3]['price']
+		data["Crossword URL"]=result[3]['url']
+		data["Landmark on the net Price"]=result[4]['price']
+		data["Landmark on the net URL"]=result[4]['url']
+		data["Infibeam Price"]=result[5]['price']
+		data["Infibeam URL"]=result[5]['url']
+	else:
+		data["Status"]="Failed/Incorrect ISBN"
 	return json.dumps(data)
