f3d1f59c1c0e205c4304daf7362255c06806c846
Teja Theegala <sriteja23@gmail.com>
2015-04-10 17:06:56 -0700

    Better Error Handling

--- bookbargain_api/__init__.py
+++ bookbargain_api/__init__.py
@@ -26,9 +26,9 @@ def api():
 	args = [(target, isbn) for target in (flipkart,amazon,uread,crossword,landmark,infibeam)]
 	result = pool.map(worker, args)
 
-
 	data = OrderedDict()
-	if(result[0]['name']!="NA"):
+
+	if(result[0]['status']=="success"):
 		data["Status"]="Success"
 		data["Book Name"]=result[0]['name']
 		data["Author"]=result[0]['author']
@@ -36,17 +36,32 @@ def api():
 		data["Image"]=result[0]['image_url']
 		data["Flipkart Price"]=result[0]['price']
 		data["Flipkart URL"]=result[0]['url']
+
 		data["Amazon Price"]=result[1]['price']
-		data["Amazon URL"]=result[1]['url']
+		if(result[1]['price']!="NA"):
+			data["Amazon URL"]=result[1]['url']
+
 		data["URead Price"]=result[2]['price']
-		data["URead URL"]=result[2]['url']
+		if(result[2]['price']!="NA"):
+			data["URead URL"]=result[2]['url']
+
 		data["Crossword Price"]=result[3]['price']
-		data["Crossword URL"]=result[3]['url']
+		if(result[3]['price']!="NA"):
+			data["Crossword URL"]=result[3]['url']
+
 		data["Landmark on the net Price"]=result[4]['price']
-		data["Landmark on the net URL"]=result[4]['url']
+		if(result[4]['price']!="NA"):
+			data["Landmark on the net URL"]=result[4]['url']
+
 		data["Infibeam Price"]=result[5]['price']
-		data["Infibeam URL"]=result[5]['url']
-	else:
-		data["Status"]="Failed/Incorrect ISBN"
+		if(result[5]['price']!="NA"):
+			data["Infibeam URL"]=result[5]['url']
+
+	elif(result[0]['status']=="error_connecting"):
+		data["Status"]="Error in connecting"
+
+	elif(result[0]['status']=="invalid_isbn"):
+		data["Status"]="Invalid ISBN"
+
 	print "Time taken for the call: "+str(time.time()-start_time)
 	return json.dumps(data)
--- bookbargain_api/modules/flipkart.py
+++ bookbargain_api/modules/flipkart.py
@@ -1,32 +1,35 @@
 import urllib2
 from bs4 import BeautifulSoup
-import time
 import mechanize
 import re
 import pyisbn
 
 def flipkart(isbn):
-	#Getting the Source
-	start_time = time.time()
+
+	# Getting the Source from the link
+
 	if len(isbn) == 10:
 		f_link = "http://www.flipkart.com/search?q="+pyisbn.Isbn10(isbn).convert(code='978')
 	else:
 		f_link = "http://www.flipkart.com/search?q="+isbn
 		
-	f_link = f_link +"&affid=sriteja96"
+	f_link = f_link +"&affid=sriteja96" # Affliate ID
 	br = mechanize.Browser()
-	br.set_handle_robots(False)   # ignore robots
-	br.set_handle_refresh(False)  # can sometimes hang without this
-	br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]# [('User-agent', 'Firefox')]
+	br.set_handle_robots(False)   # Ignore robots.txt
+	br.set_handle_refresh(False)  
+	br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
 	try:
 		response = br.open(f_link)
+
 	except Exception, e:
 		f_title = "NA"
-		f_price="NA"
-		f_author="NA"
+		f_price = "NA"
+		f_author = "NA"
 		f_img = "NA"
-		f_desc="NA"
-		flip_dict = {'name':f_title,'price':f_price,'author':f_author,'image_url':f_img,'desc':f_desc,'url':f_link}
+		f_desc = "NA"
+		f_status = "error_connecting"
+		flip_dict = {'name':f_title,'price':f_price,'author':f_author,'image_url':f_img,'desc':f_desc,'url':f_link, 'status':f_status}
 		return flip_dict
 
 	#Creating the soup
@@ -57,7 +60,7 @@ def flipkart(isbn):
 			f_img = "NA"
 
 		f_desc_temp = f_soup.find_all("div",class_="description-text")
-		#print f_desc_temp
+
 		if(f_desc_temp):
 			f_desc = f_desc_temp[0].decode_contents(formatter="html")
 			soup = BeautifulSoup(f_desc)
@@ -65,15 +68,17 @@ def flipkart(isbn):
 		else:
 			f_desc="NA"
 
+		f_status = "success"
+
 	else:
 		f_title = "NA"
-		f_price="NA"
-		f_author="NA"
+		f_price = "NA"
+		f_author = "NA"
 		f_img = "NA"
-		f_desc="NA"
+		f_desc = "NA"
+		f_status =  "invalid_isbn"
 
-	print time.time()-start_time
-	flip_dict = {'name':f_title,'price':f_price,'author':f_author,'image_url':f_img,'desc':f_desc,'url':f_link}
+	flip_dict = {'name':f_title,'price':f_price,'author':f_author,'image_url':f_img,'desc':f_desc,'url':f_link,'status':f_status}
 
 	return flip_dict
 
